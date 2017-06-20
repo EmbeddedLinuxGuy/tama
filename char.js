cf73196792c92de364c23600b875fc8411c4a6ac
embeddedLinuxGuy <embeddedLinuxGuy@gmail.com>
2017-06-20 09:44:31 -0700

    add stats

--- char.js
+++ char.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
 
-var readline = require('readline');
-
-var rl = readline.createInterface({
+const readline = require('readline');
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
 });
+const R = require('ramda');
 
 const q0 = [
     "########################",
     "# Welcome to Ryuu-Tama #",
     "########################\n\n"
 ];
-
-const classes = [ "Healer", "Hunter", "Merchant", "Minstrel" ];
-const types = [ "Attack", "Magic", "Technical" ];
+process.stdout.write(q0.join("\n"));
+var choices = [];
+var step = 0;
 
 const q1 = [ "Choose a class:\n",
 	     "1. Healer",
@@ -23,6 +23,10 @@ const q1 = [ "Choose a class:\n",
 	     "3. Merchant",
 	     "4. Minstrel",
 	     "\n> " ];
+process.stdout.write(q1.join("\n"));
+
+const classes = [ "Healer", "Hunter", "Merchant", "Minstrel" ];
+const types = [ "Attack", "Magic", "Technical" ];
 
 const q = { "type":
 	    [ "\nChoose a type:\n",
@@ -44,31 +48,32 @@ const q = { "type":
 		"int (Intelligence)",
 		"spi (Spirit)",
 		"\n> "
-	    ]
+	    ],
+	    "name": [ "\nEnter a name: " ]
 	  };
 
-var choices = [];
-
-process.stdout.write(q0.join("\n"));
-process.stdout.write(q1.join("\n"));
-
-var step = 0;
+const keys = [ "type", "name", "stats", "ranks" ];
+const klass = 0;
+const type = 1;
+const name = 2;
+const stats = 3;
+const ranks = 4;
+const stat_ranks = [ [ 6, 6, 6, 6 ], [ 8, 6, 6, 4], [8, 8, 4, 4] ];
 
 rl.on('line', function (cmd) {
     choices.push(cmd);
-    ++step;
-    if (step == 1) {
-	process.stdout.write(q["type"].join("\n"));
-    } else if (step == 2) {
-	process.stdout.write("\nEnter a name: ");
-    } else if (step == 3) {
-	process.stdout.write(q["stats"].join("\n"));
-    } else if (step == 4) {
-	process.stdout.write(q["ranks"].join("\n"));
+    if (step < 4) {
+	process.stdout.write(q[keys[step]].join("\n"));
+	++step;
     } else {
-	process.stdout.write("You are " + choices[2] + " (Level 1, "
-			     + types[choices[1]-1] + "-type "
-			     + classes[choices[0]-1] + ")\n");
+	// do bounds checking
+	process.stdout.write("You are " + choices[name] + " (Level 1, "
+			     + types[choices[type]-1] + "-type "
+			     + classes[choices[klass]-1] + ")\n");
+	var st = stat_ranks[choices[stats]-1];
+	var ra = choices[ranks].split(" ");
+	var z = R.zipWith((a, b) => a.toUpperCase() + ":" + b, ra, st);
+	process.stdout.write("Stats: " + z.join(" ") + "\n");
 	process.exit(0);
     }
 //    process.stdout.write("\nYou chose: [" + cmd + "]\n");
