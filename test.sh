98992f9fef2b355ab932328ace1782b6ed20ec43
embeddedLinuxGuy <embeddedLinuxGuy@gmail.com>
2017-06-27 20:08:24 -0700

    test generated json

--- test.sh
+++ test.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+CHARGEN=./char.js
+NEW_JSON=new-char.json
+REF_JSON=test-char.json
+OUTPUT=test-result.txt
+
 if [ "$1" = "regen" ]; then
     (echo 4; echo 2; echo Guy; echo 2; echo str dex int spi) \
-	| ./char.js > test-result.txt
+	| $CHARGEN $REF_JSON > $OUTPUT
 else
     if (echo 4; echo 2; echo Guy; echo 2; echo str dex int spi) \
-	   | ./char.js | diff -u - test-result.txt; then
-	echo OK
+	   | ./char.js $NEW_JSON | diff -u - $OUTPUT; then
+	if diff -u $NEW_JSON $REF_JSON; then
+	    echo OK
+	else
+	    echo CHARGEN FAILED: output OK
+	fi
     else
-	echo Fail
+	echo OUTPUT FAILED
     fi
 fi
 
