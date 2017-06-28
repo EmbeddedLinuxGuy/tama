ab430cf594406a17327aa85d9766b9677586d5c6
embeddedLinuxGuy <embeddedLinuxGuy@gmail.com>
2017-06-27 20:07:04 -0700

    generate stat json

--- char.js
+++ char.js
@@ -1,19 +1,30 @@
 #!/usr/bin/env node
 
 const readline = require('readline');
+const fs = require("fs");
+const R = require('ramda');
+
+
+if (process.argv.length != 3) {
+    process.stderr.write("Usage: " + process.argv[1]
+			 + " outfile.json\n");
+    process.exit(1);
+}
+
+const json_out = process.argv[2];
+const tty_out = process.stdout;
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
 });
-const R = require('ramda');
 
 const q0 = [
     "########################",
     "# Welcome to Ryuu-Tama #",
     "########################\n\n"
 ];
-process.stdout.write(q0.join("\n"));
+tty_out.write(q0.join("\n"));
 var choices = [];
 var step = 0;
 
@@ -23,7 +34,7 @@ const q1 = [ "Choose a class:\n",
 	     "3. Merchant",
 	     "4. Minstrel",
 	     "\n> " ];
-process.stdout.write(q1.join("\n"));
+tty_out.write(q1.join("\n"));
 
 const classes = [ "Healer", "Hunter", "Merchant", "Minstrel" ];
 const types = [ "Attack", "Magic", "Technical" ];
@@ -63,18 +74,20 @@ const stat_ranks = [ [ 6, 6, 6, 6 ], [ 8, 6, 6, 4], [8, 8, 4, 4] ];
 rl.on('line', function (cmd) {
     choices.push(cmd);
     if (step < 4) {
-	process.stdout.write(q[keys[step]].join("\n"));
+	tty_out.write(q[keys[step]].join("\n"));
 	++step;
     } else {
 	// do bounds checking
-	process.stdout.write("You are " + choices[name] + " (Level 1, "
-			     + types[choices[type]-1] + "-type "
-			     + classes[choices[klass]-1] + ")\n");
-	var st = stat_ranks[choices[stats]-1];
-	var ra = choices[ranks].split(" ");
-	var z = R.zipWith((a, b) => a.toUpperCase() + ":" + b, ra, st);
-	process.stdout.write("Stats: " + z.join(" ") + "\n");
+	const zo = R.zipObj(choices[ranks].split(" "),
+			    stat_ranks[choices[stats]-1]);
+
+	tty_out.write("You are " + choices[name] + " (Level 1, "
+		      + types[choices[type]-1] + "-type "
+		      + classes[choices[klass]-1] + ")\n"
+		      + "Stats: "
+		      + JSON.stringify(zo)
+		      + "\n");
+	fs.writeFileSync(json_out, JSON.stringify(zo));
 	process.exit(0);
     }
-//    process.stdout.write("\nYou chose: [" + cmd + "]\n");
 });
